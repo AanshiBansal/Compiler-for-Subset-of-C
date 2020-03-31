80001f3ffcf86e08a520e2dbbe5e48aed51a3027
Ishita Agarwal <iagarwal7@gmail.com>
2020-03-31 16:33:58 +0530

    initial

---
+++ myscanner.h
@@ -0,0 +1,6 @@
+#define IDENTIFIER 1  //any variable name, function name
+#define INTEGER 2  //unsigned integer only
+#define DATATYPE 3 //like int, char etc
+#define INCLUDE 4
+#define RETURN 5
+#define HEADER_NAME 6
